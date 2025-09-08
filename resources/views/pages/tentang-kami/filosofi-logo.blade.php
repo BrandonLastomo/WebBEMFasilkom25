44af65ed598529180eb39a47978b2bb40bc4b376
Brandon <brandonlastomo1@gmail.com>
2025-09-09 00:48:30 +0700

    redesign struktur UI and minor fix

--- resources/views/pages/tentang-kami/filosofi-logo.blade.php
+++ resources/views/pages/tentang-kami/filosofi-logo.blade.php
@@ -6,15 +6,11 @@
 
 @section('content')
     <!-- Logo Section -->
-    <!-- Logo Section -->
-    <section id="logo" class="logo mt-5"
-        style="margin: 0; padding: 0; padding-top:40px; height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <section id="logo" class="filosofiLogoFull mt-5">
 
-        <img src="{{ url('frontend/assets/img/logo-kabinet/filosofi-logo.png') }}" class="logo-arthachara"
-            style="max-width: 90vw; height: 100%; width: auto; transform: scale(10);">
+        <img src="{{ url('frontend/assets/img/logo-kabinet/filosofi-logo.png') }}" class="logo-arthachara">
 
     </section>
-    <!-- End Logo Section -->
 
     <!-- End Logo Section -->
 @endsection
